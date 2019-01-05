dc389255f40c89d6522c531e64a08d4fcdf43339
MuziZim <35012559+MuziZim@users.noreply.github.com>
2019-01-05 11:27:31 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix creates special matrix object that can cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {       #initiate the function by setting x (input) to an empty matrix
+  mTrix <- NULL                                   #mTrix will be the inverted matrix that gets returned
+  calc <- function(y) {                           #initiate the actual calculation
+    x <<- y                                       #store the output in different environment
+    mTrix <<- NULL                                #mTrix stored in different environment (still Null)
+  }
+  ret <- function() x                             
+  set <- function() mTrix <<- solve(x)            #calculate the inverse and store in separate environment
+  obt <- function() mTrix                         #obtain the value of the inverse matrix
+  list(calc = calc, ret = ret, set = set, get = get)
+  
 }
 
+##casheSolve solves function above - if already solved AND MATRIX HASN'T CHANGED, retrieve from cache
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {                  #solve inverse function - input is matrix x
+  
+  mTrix <- x$obt()                                #return the value of the inverse matrix (makeCacheMatrix function)
+  if (!is.null(mTrix)) {                          #if the cached matrix is not null, then obtain that value
+    message("getting cached data")
+    return(mTrix)
+  }
+  mat <- x$ret()
+  mTrix <- solve(mat, ...)
+  x$set(mTrix)
+  mTrix
 }
+
